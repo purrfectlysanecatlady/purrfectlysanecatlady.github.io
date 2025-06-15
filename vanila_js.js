90a306820f38dd25c675f2bb1f6168400127e254
purrfectlysanecatlady <purrfectlysanecatlady@mail.ru>
2025-06-15 14:31:48 +0600

    Add files via upload

---
+++ vanila_js.js
@@ -0,0 +1,24 @@
+const reactContentRoot = document.getElementById('root')
+
+// const myItem = "mike"
+
+const App = () => {
+    const myItem = "mike"
+    return (
+        <ul>
+            <li>Item1</li>
+            <li>Item2</li>
+            <li>{myItem.toUpperCase()}</li>
+        </ul>)
+}
+
+// const myJSXElement = (
+//     <ul>
+//         <li>Item1</li>
+//         <li>Item2</li>
+//         <li>{myItem}</li>
+//     </ul>
+// // )
+// ReactDOM.render(myJSXElement, reactContentRoot)
+
+ReactDOM.render(<App />, reactContentRoot)
